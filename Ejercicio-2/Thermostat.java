6a0cc307caabcd1eed88c9dd603d7e46d84cd1c0
RaulCM <raulcanomontero@hotmail.com>
2017-12-30 20:42:59 +0100

    Realizado ejercicio 2

---
+++ Ejercicio-2/Thermostat.java
@@ -0,0 +1,22 @@
+public class Thermostat{
+	static String partOfDay = new String("Wake");
+	static String temp = new String("Low");
+
+	public static void advance(){
+		if (partOfDay == "Wake"){
+			partOfDay = "Sleep";
+		} else if (partOfDay == "Sleep"){
+			partOfDay = "Wake";
+		}
+	}
+	public static void up(){
+		if (temp == "Low"){
+			temp = "High";
+		}
+	} 
+	public static void down(){
+		if (temp == "High"){
+			temp = "Low";
+		}
+	}
+}
---
+++ Ejercicio-2/ThermostatTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class ThermostatTest{
+
+	@Before
+	public void setUpStreams(){
+		Thermostat.partOfDay = "Wake";
+		Thermostat.temp = "Low";
+	}
+	
+	@Test
+	public void test1(){
+		Thermostat.advance();
+		Thermostat.up();
+		Thermostat.advance();
+		Thermostat.down();
+		assertEquals("Wake Low", Thermostat.partOfDay + " " + Thermostat.temp);
+	}
+}
